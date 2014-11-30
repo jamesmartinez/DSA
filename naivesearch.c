5bcb090b5801a4777e8e08b32a1234534a9461d9
James Martinez <james@websorcery.net>
2014-11-30 15:19:37 -0600

    renamed binarysearch to naivesearch

--- naivesearch.c
+++ naivesearch.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int bsearch(int num, int size, int *list)
+int naive_search(int num, int size, int *list)
 {
      int i; 
      for(i = 0; i < size ; i++)
@@ -15,5 +15,5 @@ int bsearch(int num, int size, int *list)
 int main(void)
 {
    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-   printf("%d\n",i[bsearch(7,10,i)]);
+   printf("%d\n",i[naive_search(7,10,i)]);
 }

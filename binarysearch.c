c3d5da74425318528c6fbc04929ea282daaf7b10
James Martinez <james@websorcery.net>
2014-11-30 15:32:09 -0600

    started of binarysearch

---
+++ binarysearch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int binary_search(int num, int size, int *list)
+{
+   int start = 0;
+   int end = size;
+   
+   return 0;
+}
+
+int main(void)
+{
+   int i[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+   printf("%d\n",i[binary_search(7,10,i)]);
+}
